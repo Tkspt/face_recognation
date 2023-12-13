4d680d0356d69c2144766d2ec13f077ec283a37a
Mor Codou <morcodou.seck@ldb.sn>
2023-12-13 19:53:49 +0000

    bring some changes in tuning

--- classes/ml_face_recognizer.py
+++ classes/ml_face_recognizer.py
@@ -7,7 +7,9 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import EarlyStopping
-from kerastuner.tuners import RandomSearch
+import tensorflow as tf
+import keras_tuner as kt
+from keras.optimizers import Adam
 
 class MlFaceRecognizer:
     def __init__(self):
@@ -54,54 +56,59 @@ class MlFaceRecognizer:
     
     def _create_model(self):
         def build_model(hp):
-            model = Sequential()
+            output = len(self.class_names)
             nb_neurons1 = hp.Int('nb_neurons1', min_value=20, max_value=128, step=20)
             nb_neurons2 = hp.Int('nb_neurons2', min_value=20, max_value=128, step=20)
             dense_neurons = hp.Int('dense_neurons', min_value=100, max_value=300, step=20)
-            dropout = hp.Float('dropout_1', min_value=0.3, max_value=0.5, step=0.1)
-            has_dropout = hp.Boolean("dopout")
-            epochs = hp.Int('epochs', min_value=5, max_value=15, step=1, default=10)
+            epochs = hp.Int('epochs', min_value=5, max_value=20, step=1, default=10)
             learning_rate = hp.Choice('learning_rate', values=[0.001, 0.0001, 0.00001])
-            output = 5
-
-            model.add(Conv2D(nb_neurons1, return_sequences=True))
-
+            has_dropout = hp.Boolean("dopout")
+            if has_dropout :
+                dropout = hp.Float('dropout_1', min_value=0.3, max_value=0.5, step=0.1)
+            
+            Adam(learning_rate=learning_rate)
+            
+            model = Sequential()
+            model.add(Conv2D(nb_neurons1, (3, 3), activation='relu', input_shape=(220, 220, 3)))
             if has_dropout:
                 model.add(Dropout(dropout))
-
-            model.add(Conv2D(nb_neurons2))
-
+            model.add(Conv2D(nb_neurons2, (3, 3), activation='relu'))
             if has_dropout:
                 model.add(Dropout(dropout))
-
             model.add(Flatten())
-
             model.add(Dense(dense_neurons, activation='relu'))
-
             model.add(Dense(output, activation='softmax'))
             
-            model.compile(
-                loss='binary_crossentropy',
-                optimizer=tf.keras.optimizers.RMSprop(learning_rate=learning_rate),
-                metrics=['accuracy'],
-            )
+            model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
             return model
 
-        tuner = RandomSearch(
+        epochs = 20
+        early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+        
+        # Instancions le tuner
+        tuner = kt.RandomSearch(
             build_model,
             objective='val_accuracy',
             max_trials=3,
+            overwrite = True,
             directory='tuner_logs',
-            project_name='my_text_classification'
+            project_name='image_recognation'
         )
 
-        tuner.search(self.train_generator, epochs=10, validation_data=self.val_generator, batch_size=64)
+        # Exécutons la recherche des paramètres
+        tuner.search(
+            self.train_generator,
+            validation_data=self.val_generator,
+            epochs=epochs,
+            callbacks=[early_stopping]
+        )
+        
         best_hps = tuner.get_best_hyperparameters()[0]
 
         self.tuner = tuner
         self.model = tuner.hypermodel.build(best_hps)
-
+        print("model creation complete ....")
 
     # def _create_model(self):
     #     output = len(self.class_names)
@@ -124,17 +131,11 @@ class MlFaceRecognizer:
     #     print("model creation complete ....")
            
     def _train_model(self):
-        early_stopping = EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+        best_hps = self.tuner.get_best_hyperparameters()[0]
         
-        # Compilation du modèle ...
-        self.model.compile(
-            optimizer='adam',
-            loss='categorical_crossentropy',
-            metrics=['accuracy']
-        )
-
         # Entrainement du modèle ...
-        epochs = 20
+        # epochs = 20
+        epochs = best_hps['epochs']
         history = self.model.fit(
             self.train_generator,
             epochs=epochs,
